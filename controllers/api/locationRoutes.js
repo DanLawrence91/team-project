4338acc0e3fd8df0cee1704a91f3919d564c603e
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-03-24 10:16:06 +1030

    team and location routes for homepage written

--- controllers/api/locationRoutes.js
+++ controllers/api/locationRoutes.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const { Location } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const locationData = await Location.findAll();
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,19 @@
 const sequelize = require("../config/connection");
-const { Location, Team } = require("../models");
+const { Location, Team, User } = require("../models");
 
 const teamData = require("./teamData.json");
 const locationData = require("./locationData.json");
+const userData = require("./userData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await Team.bulkCreate(teamData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
