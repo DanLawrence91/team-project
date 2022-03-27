936975e13b2ee393c77bbdb8799692c5df0385a7
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-03-27 22:17:12 +1030

    fixed submit bug of not responding

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,8 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    let email = document.querySelector('#email-login').value.trim();
+    let password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
         // Send a POST request to the API endpoint
@@ -25,7 +25,7 @@ const loginFormHandler = async (event) => {
 
 document
 .querySelector('#loginBtn')
-.addEventListener('submit', loginFormHandler);
+.addEventListener('click', loginFormHandler);
 
 // sign-up section
 const signupFormHandler = async (event) => {
