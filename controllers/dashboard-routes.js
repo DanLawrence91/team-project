8d4976b6ab05f7c59440b4949a4d2fb29bf1e9c0
Matt DSena <matthewdsena@gmail.com>
2022-03-28 21:36:16 +1030

    routes dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,52 +1,127 @@
 // Dashboard Routes to get and edit posts
 
 const router = require("express").Router();
-const { LocationReview, TeamReview } = require("../models");
+const { User, LocationReview } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", withAuth, (req, res) => {
-  LocationReview.findAll({
-    where: {
-      userId: req.session.userId,
-    },
-  })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
+router.get("/", withAuth, async (req, res) => {
+  try {
 
-      res.render("all-posts-admin", {
-        layout: "dashboard",
-        posts,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.redirect("login");
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: LocationReview }],
     });
-});
 
-router.get("/new", withAuth, (req, res) => {
-  res.render("new-post", {
-    layout: "dashboard",
-  });
-});
+    const user = userData.get({ plain: true });
 
-router.get("/edit/:id", withAuth, (req, res) => {
-  LocationReview.findByPk(req.params.id)
-    .then((dbPostData) => {
-      if (dbPostData) {
-        const post = dbPostData.get({ plain: true });
-
-        res.render("edit-post", {
-          layout: "dashboard",
-          post,
-        });
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((err) => {
-      res.status(500).json(err);
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+//router.get("/", withAuth, async (req, res) => {
+  //try {
+    //const locationReview = await LocationReview.findAll({
+      
+   // });
+   // const locationReviews = locationReview.map((review) => review.get({plain: true}));
+    // alert(locationReviews);
+   // res.render ('dashboard', {
+   //   ...locationReviews,
+   // logged_in: req.session.logged_in
+ // });
+
+ // } catch(err) {
+ //   res.status(500),json(err);
+ // }
+//});
+
+//router.get("/", withAuth, async (req, res) => {
+  //try {
+   // const teamReview = await TeamReview.findAll({
+      
+    //});
+    //const teamReviews = teamReview.map((review) => review.get({plain: true}));
+    // alert(teamReviews);
+   // res.render ('dashboard', {
+   //   teamReviews,
+   // logged_in: req.session.logged_in
+ // });
+
+  //} catch(err) {
+  //  res.status(500),json(err);
+ // }
+// });
+
+  // LocationReview.findAll({
+  //   where: {
+  //     userId: req.session.userId,
+  //   },
+  // })
+  //   .then((dbPostData) => {
+  //     const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+  //     res.render("dashboard", {
+  //       layout: "dashboard",
+  //       posts,
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.redirect("login");
+  //   });
+//});
+
+// router.get("/", withAuth, (req, res) => {
+//   TeamReview.findAll({
+//     where: {
+//       userId: req.session.userId,
+//     },
+//   })
+//     .then((dbPostData) => {
+//       const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+//       res.render("dashboard", {
+//         layout: "dashboard",
+//         posts,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.redirect("login");
+//     });
+// });
+
+
+
+
+//router.get("/new", withAuth, (req, res) => {
+ // res.render("new-post", {
+  //  layout: "dashboard",
+ // });
+//});
+
+//router.get("/edit/:id", withAuth, (req, res) => {
+ // LocationReview.findByPk(req.params.id)
+  //  .then((dbPostData) => {
+  //    if (dbPostData) {
+  //      const post = dbPostData.get({ plain: true });
+
+    //    res.render("edit-post", {
+   //       layout: "dashboard",
+   //       post,
+  //      });
+   //   } else {
+   //     res.status(404).end();
+   //   }
+   // })
+   // .catch((err) => {
+   //   res.status(500).json(err);
+   // });
+// });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api");
 const locationRoutes = require("./getLocationRoutes");
 const teamRoutes = require("./getTeamRoutes");
+const dashboard = require("./dashboard-routes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/location", locationRoutes);
 router.use("/team", teamRoutes);
+router.use("/dashboard", dashboard );
 
 module.exports = router;
