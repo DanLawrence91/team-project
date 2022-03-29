1ab011d50f43e2852a0c793d4ae9ab451c003206
Matt DSena <matthewdsena@gmail.com>
2022-03-29 14:43:05 +1030

    click on teams and locations

--- public/script.js
+++ public/script.js
@@ -1,4 +1,20 @@
-
-
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
 timeline.from(".navbar", { y: "-100%", ease: "bounce" });
+
+function myFunction() {
+  var x = document.getElementById("location");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Function() {
+  var x = document.getElementById("team");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
