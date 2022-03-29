1162600e2d9936f15a9ff9fb3e5b303b5253ae0f
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-03-29 14:51:57 +1030

    addind matt edits to file

--- public/script.js
+++ public/script.js
@@ -1,2 +1,20 @@
 const timeline = gsap.timeline({ defaults: { duration: 1.5 } });
 timeline.from(".gsap", { y: "-100%", ease: "bounce" });
+
+function myFunction() {
+  var x = document.getElementById("location");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Function() {
+  var x = document.getElementById("team");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
